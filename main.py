dc782a5bce442f69f77dc797c07bf97b04a4d636
mantertius <31227589+mantertius@users.noreply.github.com>
2021-10-26 02:19:58 -0300

    Adiciona comandos slash ao bot, com o uso dos Cogs
    Além disso, foram algumas horas para fazer funcionar. Aprendemos como funcionam os cogs e também os slash commands.
    Foi bem divertido fazer isso.

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ import os
 from online import keep_alive
 from disnake.ext import commands
 
+main_server = 857312583898759169
+
 intents = disnake.Intents.default()
 intents.members = True
 intents.reactions = True
-bot = commands.Bot(command_prefix='!',intents=intents,activity=disnake.Game(name='em desenvolvimento'))
+
+bot = commands.Bot(command_prefix='!',intents=intents,activity=disnake.Game(name='em desenvolvimento'), test_guilds=[main_server],
+sync_commands_debug=True)
 
 for filename in os.listdir("cogs"):
     if filename.endswith(".py"):
