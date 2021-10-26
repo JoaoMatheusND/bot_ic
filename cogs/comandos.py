3d03c9911d1f5618b85d9c20f3349cad9b5b5ef0
Manoel <31227589+mantertius@users.noreply.github.com>
2021-10-26 04:57:53 +0000

    getting to know slash commands better!

--- cogs/comandos.py
+++ cogs/comandos.py
@@ -5,77 +5,108 @@ import resources
 from disnake.ext import commands
 from replit import db
 
+def converterRole(self,roleStr):
+  return disnake.ext.commands.RoleConverter(roleStr)
 
 class Commands(commands.Cog):
-  def __init__(self, client):
-    self.bot = client
-  
-  @commands.command()
-  async def ajuda(self, ctx):
-    embeds.ajuda.set_author(
-      name = ctx.author.display_name,
-      icon_url = ctx.author.display_avatar)  
-    await ctx.channel.send(embed=embeds.ajuda)
+    def __init__(self, client):
+        self.bot = client
 
-  @commands.command()
-  async def cargo(self, ctx, role:commands.RoleConverter):
-    """Recebe um nome de um cargo com caracteres sensíveis e mostra uma lista daqueles que fazem parte deste cargo."""
-    if role == Exception:
-      await ctx.send(f'_Não foi possível encontrar tal cargo._')
-      return
+    @commands.slash_command(description="Mensagem de Teste")
+    async def ajuda(self, inter, arg: str):
+        embeds.ajuda.set_author(name=inter.author.display_name,
+                                icon_url=inter.author.display_avatar)
+        await inter.response.send_message(embed=embeds.ajuda)
 
-    mentionList = []
-    memberlist = role.members
-    memberlist = sorted(memberlist,key=operator.attrgetter('display_name'))
-    for member in memberlist: #role.members é uma lista de Members.
-      mentionList.append(member.mention)
-    string = "aaa".join([element for element in mentionList])
-    string = string.replace("aaa", "\n")
+    @commands.slash_command(description="Multiplies the number by 7")
+    async def multiply(self, inter, number: float):
+        await inter.response.send_message(f'{float(number * 7)}')
 
-    embed = disnake.Embed(
-      title=role.name.upper(),
-      description= f'Usuários que são **{role.name.upper()}** no servidor!',
-      colour=embeds.cor
-      )
-    embed.add_field(name=f'Lista de {role.name} [{len(mentionList)}]', value=string, inline=True)
-    embed.set_thumbnail(url=embeds.thumb_ic)
-    embed.set_author(
-      name = ctx.author.display_name,
-      icon_url = ctx.author.display_avatar)  
-    
-    await ctx.send(embed=embed)
 
-  @commands.command()
-  async def helloworld(self, ctx):
-    await ctx.send(embed=embeds.helloworld)
 
-  @commands.command()
-  async def convite(self, ctx):
-    await ctx.reply(
-      f'Olá {ctx.author.mention}, aqui está o link de convite para o servidor:\n https://discord.gg/TBKwV2BKQg')
+    @commands.slash_command(description = "mostra uma lista daqueles que fazem parte deste cargo.")
+    async def cargo(self, inter, role: disnake.Role): 
+      # = commands.Param(conv=lambda inter, cargo: converterRole(cargo)) 
+      # = commands.Param(converter = lambda inter, arg: commands.RoleConverter.convert(inter,arg))
+        # """Recebe um nome de um cargo com caracteres sensíveis e mostra uma lista daqueles que fazem parte deste cargo."""
+        # role = await disnake.ext.commands.RoleConverter.convert(self,inter,cargo)
+        if role == Exception:
+            await inter.response.send_message(
+                f'_Não foi possível encontrar tal cargo._')
+            return
 
-  @commands.command()
-  async def Lista(self, ctx):
-    lista = disnake.Embed(title='Lista de coisas a fazer', description='Reagir com o emote 🔴 pra "pendente", 🟡 pra "em progresso" e 🟢 "pra concluído"', colour=embeds.cor)
-    lista.set_thumbnail(url=embeds.thumb_ic)
-    lista.add_field(name=f"{resources.semaforo[0]}", value=f'{db["r"]}z', inline=False)
-    lista.add_field(name=f"{resources.semaforo[1]}", value=f'{db["y"]}z', inline=False)
-    lista.add_field(name=f"{resources.semaforo[2]}", value=f'{db["g"]}z', inline=False)
-    await ctx.send(embed=lista)
-          
+        mentionList = []
+        memberlist = role.members
+        print(memberlist)
+        memberlist = sorted(memberlist,
+                            key=operator.attrgetter('display_name'))
+        for member in memberlist:  #role.members é uma lista de Members.
+            mentionList.append(member.mention)
+        string = "aaa".join([element for element in mentionList])
+        string = string.replace("aaa", "\n")
 
-  @commands.command()
-  async def grupo(self, ctx, title:str, description:str, amount:int, data:str='sem data'): 
-    embed = embeds.ourEmbed(title, description, ctx)
-    embed.set_footer(text=f'entragar em {data}')
-    for x in range(amount):
-      embed.add_field(name=f'grupo{x+1}', value='participantes: ', inline=False)
+        embed = disnake.Embed(
+            title=role.name.upper(),
+            description=
+            f'Usuários que são **{role.name.upper()}** no servidor!',
+            colour=embeds.cor)
+        embed.add_field(name=f'Lista de {role.name} [{len(mentionList)}]',
+                        value=string,
+                        inline=True)
+        embed.set_thumbnail(url=embeds.thumb_ic)
+        embed.set_author(name=inter.author.display_name,
+                         icon_url=inter.author.display_avatar)
 
-    msg = await ctx.send(embed=embed)
+        await inter.response.send_message(embed=embed)
 
-    for x in range(amount):
-      await msg.add_reaction(emoji=resources.emoji[x])
+    @commands.slash_command()
+    async def helloworld(self, inter):
+        await inter.response.send_message(embed=embeds.helloworld)
+
+    @commands.slash_command()
+    async def convite(self, inter):
+        await inter.reply(
+            f'Olá {inter.author.mention}, aqui está o link de convite para o servidor:\n https://discord.gg/TBKwV2BKQg'
+        )
+
+    @commands.slash_command()
+    async def lista(self, inter):
+        lista = disnake.Embed(
+            title='Lista de coisas a fazer',
+            description=
+            'Reagir com o emote 🔴 pra "pendente", 🟡 pra "em progresso" e 🟢 "pra concluído"',
+            colour=embeds.cor)
+        lista.set_thumbnail(url=embeds.thumb_ic)
+        lista.add_field(name=f"{resources.semaforo[0]}",
+                        value=f'{db["r"]}z',
+                        inline=False)
+        lista.add_field(name=f"{resources.semaforo[1]}",
+                        value=f'{db["y"]}z',
+                        inline=False)
+        lista.add_field(name=f"{resources.semaforo[2]}",
+                        value=f'{db["g"]}z',
+                        inline=False)
+        await inter.response.send_message(embed=lista)
+
+    @commands.slash_command()
+    async def grupo(self,
+                    inter,
+                    title: str,
+                    description: str,
+                    amount: int,
+                    data: str = 'sem data'):
+        embed = embeds.ourEmbed(title, description, inter)
+        embed.set_footer(text=f'entragar em {data}')
+        for x in range(amount):
+            embed.add_field(name=f'grupo{x+1}',
+                            value='participantes: ',
+                            inline=False)
+
+        msg = await inter.response.send_message(embed=embed)
+
+        for x in range(amount):
+            await msg.add_reaction(emoji=resources.emoji[x])
+
 
 def setup(bot):
-  bot.add_cog(Commands(bot))
-
+    bot.add_cog(Commands(bot))
